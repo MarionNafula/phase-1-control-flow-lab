2d50497b4d7039902b5b8a947b2eb1fe1ab29959
Marion <wekesamarion100@gmail.com>
2024-09-26 11:24:10 +0300

    completed Lab

--- index.js
+++ index.js
@@ -1,49 +1,42 @@
+
 function scuberGreetingForFeet(ride){
-
-  if(ride <= 400){
-    return 'This one is on me!';
-}
-
-else if(ride >2000 && ride <= 2500){
-  return 'I will gladly take your thirty bucks.';
-}
-
-else{
-  return 'No can do.'
-}
-
-}
-
-function ternaryCheckCity(){
-
-//function ternaryCheckCity(){
-  //Write your code here!
-}
-function ternaryCheckCity(city){
-if (city=== 'NYC'){
-  return 'Ok, sounds good.'
-}
-else{
-  return 'No go.'
-}
-}
-//}
-
-function switchOnCharmFromTip(){
   // Write your code here!
+  
+  if (ride > 2500) {
+    return 'No can do.';
+  }
+  else if (ride > 2000) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (ride > 400) {
+    return 'That will be twenty bucks.';
+  }
+  else {
+    return 'This one is on me!';
+  }
+
+}
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  if (city === 'NYC') {
+    return 'Ok, sounds good.';
+  }
+  else {
+    return 'No go.';
+  }
 }
 
 function switchOnCharmFromTip(tip){
-  if(tip==='generous'){
-    return 'Thank you so much.'
+  // Write your code here!
+  if (tip === 'generous') {
+    return 'Thank you so much.';
   }
-  else if(tip==='not as generous'){
-    return 'Thank you.'
+  else if (tip === 'not as generous') {
+    return 'Thank you.';
   }
-  else{
-    'Thank for everything bye.'
-    return 'Bye.'
+  else {
+    return 'Bye.';
   }
-
 }
 //}
